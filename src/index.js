9ed814f0a818097d1223e16eb7583a49eb45dc32
Pikabys <lazered68733@gmail.com>
2026-05-25 22:46:54 -0700

    Delete src directory

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import * as ReactDOMClient from "react-dom/client"
-import "./components/main.css"
-import App from "./components/App"
-const app = ReactDOMClient.createRoot(document.getElementById("main"))
-app.render(<App/>)
